281b5f6ccfc3ffe22a0393a79322bce85a47250e
mouhamed2013 <bouyahia13@hotmail.fr>
2014-10-25 15:01:38 +0200

    Update badges.php

--- badges.php
+++ badges.php
@@ -2,7 +2,8 @@
 
 #|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|
 #|                                                                        #|
-#|         HABBOPHP - http://habbophp.com                                 #|
+#|         HABBOPHP
+- http://habbobeta.fr                             #|
 #|         Copyright © 2012 Valentin & Robin. All rights reserved.        #|
 #|																		  #|
 #|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|
@@ -19,4 +20,4 @@ $tpl->display('header.tpl');
 $tpl->display('badges.tpl');	
 $tpl->display('footer.tpl');
 
-?>
+?>http://www.google.dz/imgres?imgurl=http%3A%2F%2Fadodreams.e-monsite.com%2Fmedias%2Fimages%2F22612332badge-habbo-staff-1-gif.gif%253Ffx%253Dr_120_120&imgrefurl=http%3A%2F%2Fadodreams.e-monsite.com%2Fpages%2Fquizze-sur-habbo.html&h=120&w=120&tbnid=WVeXmLMm5zOb2M%3A&zoom=1&docid=jEgZVoLMeX9vrM&hl=fr&ei=6J5LVKT-DdDias_OgDg&tbm=isch&ved=0CFkQMyg0MDQ&iact=rc&uact=3&dur=1351&page=3&start=48&ndsp=27
